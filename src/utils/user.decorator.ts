6da9eac865792b756a5bda011948f79242e9b9d9
weixiao <2216694426@qq.com>
2024-02-25 16:50:00 +0800

    使用数据权限的接口不缓存

--- src/utils/user.decorator.ts
+++ src/utils/user.decorator.ts
@@ -1,6 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { NoCache } from 'src/cache/my-cache.interceptor';
 
 export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
+  //使用数据权限的接口不缓存
+  NoCache()
   const request = ctx.switchToHttp().getRequest();
   return request.user;
 });
